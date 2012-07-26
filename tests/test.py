55370008ce5c3f84058496fd3f076f6252d45a1f
Andrew Straw <strawman@astraw.com>
2012-07-26 15:19:49 +0200

    test floating point data

--- tests/test.py
+++ tests/test.py
@@ -4,13 +4,13 @@ import tempfile
 import pcl
 import numpy as np
 
-_data = [(i,2*i,3*i) for i in range(5)]
+_data = [(i,2*i,3*i+0.2) for i in range(5)]
 _DATA = \
-"""0.0, 0.0, 0.0;
-1.0, 2.0, 3.0;
-2.0, 4.0, 6.0;
-3.0, 6.0, 9.0;
-4.0, 8.0, 12.0"""
+"""0.0, 0.0, 0.2;
+1.0, 2.0, 3.2;
+2.0, 4.0, 6.2;
+3.0, 6.0, 9.2;
+4.0, 8.0, 12.2"""
 
 class TestListIO(unittest.TestCase):
 
