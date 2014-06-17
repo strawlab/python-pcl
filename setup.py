e3e2443f8c0d51874e6f9077758f119f47384156
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-06-17 16:57:44 +0200

    Check for various PCL versions at compile time

--- setup.py
+++ setup.py
@@ -1,21 +1,35 @@
+from __future__ import print_function
 from collections import defaultdict
 from Cython.Distutils import build_ext
 from distutils.core import setup
 from distutils.extension import Extension
 import subprocess
+import sys
 
-PCL_VER = "1.7"
+
+# Try to find PCL. XXX we should only do this when trying to build or install.
+PCL_SUPPORTED = ["-1.7", "-1.6", ""]    # in order of preference
+
+for pcl_version in PCL_SUPPORTED:
+    if subprocess.call(['pkg-config', 'pcl_common%s' % pcl_version]) == 0:
+        break
+else:
+    print("%s: error: cannot find PCL, tried" % sys.argv[0], file=sys.stderr)
+    for version in PCL_SUPPORTED:
+        print('    pkg-config pcl_common%s' % version, file=sys.stderr)
+    sys.exit(1)
 
 # Find build/link options for PCL using pkg-config.
 pcl_libs = ["common", "features", "filters", "io", "kdtree", "octree",
             "sample_consensus", "search", "segmentation", "surface"]
-pcl_libs = ["pcl_%s-%s" % (lib, PCL_VER) for lib in pcl_libs]
+pcl_libs = ["pcl_%s%s" % (lib, pcl_version) for lib in pcl_libs]
 
 ext_args = defaultdict(list)
 
 
 def pkgconfig(flag):
     return subprocess.check_output(['pkg-config', flag] + pcl_libs).split()
+
 
 for flag in pkgconfig('--cflags-only-I'):
     ext_args['include_dirs'].append(flag[2:])
