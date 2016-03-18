b87e022bfe5bf0e195666303aef745ca1d85a7eb
sirokujira <toru753@gmail.com>
2016-03-18 15:21:53 +0900

    modified2

--- clear.sh
+++ clear.sh
@@ -1,12 +1,12 @@
-del pcl\_pcl.cpp
-del pcl\_pcl.pyd
-del pcl\pcl_registration_160.cpp
-del pcl\pcl_registration_160.pyd
-del pcl\pcl_registration_172.cpp
-del pcl\pcl_registration_172.pyd
-del pcl\pcl_visualization.cpp
-del pcl\pcl_visualization.pyd
-del pcl\pcl_grabber.cpp
-del pcl\pcl_grabber.pyd
-rd /s /q build
+rm pcl/_pcl.cpp
+rm pcl/_pcl.pyd
+rm pcl/pcl_registration_160.cpp
+rm pcl/pcl_registration_160.pyd
+rm pcl/pcl_registration_172.cpp
+rm pcl/pcl_registration_172.pyd
+rm pcl/pcl_visualization.cpp
+rm pcl/pcl_visualization.pyd
+rm pcl/pcl_grabber.cpp
+rm pcl/pcl_grabber.pyd
+rm -rf build
 pip uninstall python-pcl -y
