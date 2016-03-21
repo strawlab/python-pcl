5a2041d2a4ad6312d0d631d32e3c6eb6df2ee086
sirokujira <toru753@gmail.com>
2016-03-21 09:08:46 +0900

    commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 from Cython.Distutils import build_ext
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
+# from Cython.Build import cythonize	# MacOS NG
 import subprocess
 import numpy
 import sys
