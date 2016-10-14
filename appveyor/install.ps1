81de13824f7db2ec4acd06da209fd6165b96acb6
sirokujira <toru753@gmail.com>
2016-10-14 18:16:00 +0900

    appveyor test

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -391,7 +391,7 @@ function InstallOpenNIMSI ($msipath, $openni_home, $install_log)
     # RunCommand "msiexec.exe" $install_args
     # task use
     # RunCommand "schtasks" "/create /tn openni_install /RL HIGHEST /tr `"msiexec.exe $install_args`" /sc once /st 23:59"
-    RunCommand "schtasks" "/create /tn openni_install /RL HIGHEST /tr `"$msipath $install_args`" /sc once /st 23:59"
+    RunCommand "schtasks" "/create /tn openni_install /RL HIGHEST /tr `"msiexec.exe /i $msipath $install_args`" /sc once /st 23:59"
     RunCommand "sleep" "10"
     RunCommand "schtasks" "/run /tn openni_install"
     RunCommand "sleep" "90"
