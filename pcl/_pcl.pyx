7767685880a8f333cdb4f8892658e727c2d869d7
rkeatin3 <keating.ryan.t@gmail.com>
2017-01-26 15:59:15 -0500

    Fixed pointcloud resize to raise correct error.

--- pcl/_pcl.pyx
+++ pcl/_pcl.pyx
@@ -47,7 +47,7 @@ SACMODEL_TORUS = cpp.SACMODEL_TORUS
 SACMODEL_PARALLEL_LINE = cpp.SACMODEL_PARALLEL_LINE
 SACMODEL_PERPENDICULAR_PLANE = cpp.SACMODEL_PERPENDICULAR_PLANE
 SACMODEL_PARALLEL_LINES = cpp.SACMODEL_PARALLEL_LINES
-SACMODEL_NORMAL_PLANE = cpp.SACMODEL_NORMAL_PLANE 
+SACMODEL_NORMAL_PLANE = cpp.SACMODEL_NORMAL_PLANE
 #SACMODEL_NORMAL_SPHERE = cpp.SACMODEL_NORMAL_SPHERE
 SACMODEL_REGISTRATION = cpp.SACMODEL_REGISTRATION
 SACMODEL_PARALLEL_PLANE = cpp.SACMODEL_PARALLEL_PLANE
@@ -303,6 +303,8 @@ cdef class PointCloud:
         if self._view_count > 0:
             raise ValueError("can't resize PointCloud while there are"
                              " arrays/memoryviews referencing it")
+        if x < 0:
+            raise MemoryError("can't resize PointCloud to negative size")
         self.thisptr().resize(x)
 
     def get_point(self, cnp.npy_intp row, cnp.npy_intp col):
@@ -436,7 +438,7 @@ cdef class PointCloud:
 
     def extract(self, pyindices, bool negative=False):
         """
-        Given a list of indices of points in the pointcloud, return a 
+        Given a list of indices of points in the pointcloud, return a
         new pointcloud containing only those points.
         """
         cdef PointCloud result
@@ -481,7 +483,7 @@ cdef class StatisticalOutlierRemovalFilter:
 
     def set_mean_k(self, int k):
         """
-        Set the number of points (k) to use for mean distance estimation. 
+        Set the number of points (k) to use for mean distance estimation.
         """
         self.me.setMeanK(k)
 
@@ -493,7 +495,7 @@ cdef class StatisticalOutlierRemovalFilter:
 
     def set_negative(self, bool negative):
         """
-        Set whether the indices should be returned, or all points except the indices. 
+        Set whether the indices should be returned, or all points except the indices.
         """
         self.me.setNegative(negative)
 
@@ -519,13 +521,13 @@ cdef class MovingLeastSquares:
 
     def set_search_radius(self, double radius):
         """
-        Set the sphere radius that is to be used for determining the k-nearest neighbors used for fitting. 
+        Set the sphere radius that is to be used for determining the k-nearest neighbors used for fitting.
         """
         self.me.setSearchRadius (radius)
 
     def set_polynomial_order(self, bool order):
         """
-        Set the order of the polynomial to be fit. 
+        Set the order of the polynomial to be fit.
         """
         self.me.setPolynomialOrder(order)
 
@@ -687,7 +689,7 @@ cdef class OctreePointCloud:
     def __init__(self, double resolution):
         """
         Constructs octree pointcloud with given resolution at lowest octree level
-        """ 
+        """
         self.me = new cpp.OctreePointCloud_t(resolution)
 
     def __dealloc__(self):
@@ -702,10 +704,10 @@ cdef class OctreePointCloud:
 
     def define_bounding_box(self):
         """
-        Investigate dimensions of pointcloud data set and define corresponding bounding box for octree. 
+        Investigate dimensions of pointcloud data set and define corresponding bounding box for octree.
         """
         self.me.defineBoundingBox()
-        
+
     def define_bounding_box(self, double min_x, double min_y, double min_z, double max_x, double max_y, double max_z):
         """
         Define bounding box for octree. Bounding box cannot be changed once the octree contains elements.
@@ -751,9 +753,9 @@ cdef class OctreePointCloudSearch(OctreePointCloud):
     def __cinit__(self, double resolution):
         """
         Constructs octree pointcloud with given resolution at lowest octree level
-        """ 
+        """
         self.me = <cpp.OctreePointCloud_t*> new cpp.OctreePointCloudSearch_t(resolution)
- 
+
     def radius_search (self, point, double radius, unsigned int max_nn = 0):
         """
         Search for all neighbors of query point that are within a given radius.
