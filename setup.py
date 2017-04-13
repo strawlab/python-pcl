f7ade9d01ecc3b7f0b4eeffb8209a32abb22ee38
sirokujira <toru753@gmail.com>
2017-04-13 13:38:13 +0900

    modified setup.py(add callback.cpp)

--- setup.py
+++ setup.py
@@ -384,7 +384,7 @@ if platform.system() == "Windows":
               tests_require=['mock', 'nose'],
               ext_modules=[Extension("pcl._pcl", ["pcl/_pcl.pyx", "pcl/minipcl.cpp", "pcl/ProjectInliers.cpp"], language = "c++", **ext_args),
                            Extension("pcl.pcl_visualization", ["pcl/pcl_visualization.pyx"], language="c++", **ext_args),
-                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx"], language="c++", **ext_args),
+                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx", "pcl/grabber_callback.cpp"], language="c++", **ext_args),
                            # debug
                            # gdb_debug=True,
                            # Cython debug ex.
@@ -408,7 +408,7 @@ if platform.system() == "Windows":
               tests_require=['mock', 'nose'],
               ext_modules=[Extension("pcl._pcl", ["pcl/_pcl_172.pyx", "pcl/minipcl.cpp", "pcl/ProjectInliers.cpp"], language = "c++", **ext_args),
                            Extension("pcl.pcl_visualization", ["pcl/pcl_visualization.pyx"], language="c++", **ext_args),
-                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx"], language="c++", **ext_args),
+                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx", "pcl/grabber_callback.cpp"], language="c++", **ext_args),
                            # debug
                            # gdb_debug=True,
                           ],
@@ -429,8 +429,7 @@ if platform.system() == "Windows":
               tests_require=['mock', 'nose'],
               ext_modules=[Extension("pcl._pcl", ["pcl/_pcl_180.pyx", "pcl/minipcl.cpp", "pcl/ProjectInliers.cpp"], language = "c++", **ext_args),
                            Extension("pcl.pcl_visualization", ["pcl/pcl_visualization.pyx"], language="c++", **ext_args),
-                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx"],
-                           #         language="c++", **ext_args),
+                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx", "pcl/grabber_callback.cpp"], language="c++", **ext_args),
                            # debug
                            # gdb_debug=True,
                           ],
@@ -526,7 +525,7 @@ else:
               tests_require=['mock', 'nose'],
               ext_modules=[Extension("pcl._pcl", ["pcl/_pcl.pyx", "pcl/minipcl.cpp", "pcl/ProjectInliers.cpp"], language = "c++", **ext_args),
                            # Extension("pcl.pcl_visualization", ["pcl/pcl_visualization.pyx"], language="c++", **ext_args),
-                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx"], language="c++", **ext_args),
+                           # Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx", "pcl/grabber_callback.cpp"], language="c++", **ext_args),
                            # debug
                            # gdb_debug=True,
                           ],
@@ -547,7 +546,7 @@ else:
               tests_require=['mock', 'nose'],
               ext_modules=[Extension("pcl._pcl", ["pcl/_pcl_172.pyx", "pcl/minipcl.cpp", "pcl/ProjectInliers.cpp"], language = "c++", **ext_args),
                            # Extension("pcl.pcl_visualization", ["pcl/pcl_visualization.pyx"], language="c++", **ext_args),
-                           Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx"], language="c++", **ext_args),
+                           Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx", "pcl/grabber_callback.cpp"], language="c++", **ext_args),
                            # debug
                            # gdb_debug=True,
                           ],
@@ -568,7 +567,7 @@ else:
               tests_require=['mock', 'nose'],
               ext_modules=[Extension("pcl._pcl", ["pcl/_pcl_180.pyx", "pcl/minipcl.cpp", "pcl/ProjectInliers.cpp"], language = "c++", **ext_args),
                            # Extension("pcl.pcl_visualization", ["pcl/pcl_visualization.pyx"], language="c++", **ext_args),
-                           Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx"], language="c++", **ext_args),
+                           Extension("pcl.pcl_grabber", ["pcl/pcl_grabber.pyx", "pcl/grabber_callback.cpp"], language="c++", **ext_args),
                            # debug
                            # gdb_debug=True,
                           ],
