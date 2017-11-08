6d80f0dddaaf066d0aba9ef3784ec3fe4aa9d7a6
sirokujira <toru753@gmail.com>
2017-11-08 12:44:16 +0900

    test modified(sampleconsensus)

--- tests/test_sampleconsensus.py
+++ tests/test_sampleconsensus.py
@@ -7,119 +7,137 @@ import unittest
 import pcl
 import numpy as np
 
+
+# _data = [(i, 2 * i, 3 * i + 0.2) for i in range(500)]
+
+import random
+_data = [(random.random (), random.random (), random.random ()) for i in range(500)]
+ 
+
 # sample_consensus
 
 ### RandomSampleConsensus ###
 class TestRandomSampleConsensus(unittest.TestCase):
     def setUp(self):
         self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
 
-    def test_Test(self):
-        pass
-
-#   def test_computeModel
-#     def computeModel(self):
-#         self.me.computeModel(0)
-# 
-#     # base Class(SampleConsensus)
-#     def set_DistanceThreshold(self, double param):
-#         self.me.setDistanceThreshold(param)
-# 
-#     # base Class(SampleConsensus)
-#     def get_Inliers(self):
-#         cdef vector[int] inliers
-#         self.me.getInliers(inliers)
-#         return inliers
+    # def test_SampleConsensusModel(self):
+    #     model = pcl.SampleConsensusModel(self.p)
+    #     ransac = pcl.RandomSampleConsensus (model)
+    #     ransac.set_DistanceThreshold (.01)
+    #     ransac.computeModel()
+    #     inliers = ransac.get_Inliers()
+    #     
+    #     # print(str(len(inliers)))
+    #     self.assertNotEqual(len(inliers), 0)
 
 
-### SampleConsensusModel ###
-class TestSampleConsensus(unittest.TestCase):
-    def setUp(self):
-        self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
+    # def test_SampleConsensusModelCylinder(self):
+    #     model_cy = pcl.SampleConsensusModelCylinder(self.p)
+    #     ransac = pcl.RandomSampleConsensus (model_cy)
+    #     ransac.set_DistanceThreshold (.01)
+    #     ransac.computeModel()
+    #     inliers = ransac.get_Inliers()
+    #     
+    #     # print(str(len(inliers)))
+    #     self.assertNotEqual(len(inliers), 0)
 
 
-    def test_Test(self):
-        pass
+    def test_SampleConsensusModelLine(self):
+        model_line = pcl.SampleConsensusModelLine(self.p)
+        ransac = pcl.RandomSampleConsensus (model_line)
+        ransac.set_DistanceThreshold (.01)
+        ransac.computeModel()
+        inliers = ransac.get_Inliers()
+        
+        # print(str(len(inliers)))
+        self.assertNotEqual(len(inliers), 0)
 
 
-### SampleConsensusModelCylinder ###
-class TestSampleConsensusModelCylinder(unittest.TestCase):
-    def setUp(self):
-        self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
+    def test_ModelPlane(self):
+        model_p = pcl.SampleConsensusModelPlane(self.p)
+        ransac = pcl.RandomSampleConsensus (model_p)
+        ransac.set_DistanceThreshold (.01)
+        ransac.computeModel()
+        inliers = ransac.get_Inliers()
 
+        # print(str(len(inliers)))
+        self.assertNotEqual(len(inliers), 0)
+        
+        final = pcl.PointCloud()
+        
+        if len(inliers) != 0:
+            finalpoints = np.zeros((len(inliers), 3), dtype=np.float32)
+            
+            for i in range(0, len(inliers)):
+                finalpoints[i][0] = self.p[inliers[i]][0]
+                finalpoints[i][1] = self.p[inliers[i]][1]
+                finalpoints[i][2] = self.p[inliers[i]][2]
 
-    def test_Test(self):
-        pass
+            final.from_array(finalpoints)
 
-
-### SampleConsensusModelLine ###
-class TestSampleConsensusModelLine(unittest.TestCase):
-    def setUp(self):
-        self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
-
-
-    def test_Test(self):
-        pass
-
-
-### SampleConsensusModelPlane ###
-class TestSampleConsensusModelPlane(unittest.TestCase):
-    def setUp(self):
-        self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
-
-
-    def test_Test(self):
+        self.assertNotEqual(final.size, 0)
         pass
 
 
-### SampleConsensusModelRegistration ###
-class TestSampleConsensusModelRegistration(unittest.TestCase):
-    def setUp(self):
-        self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
+    # def test_SampleConsensusModelRegistration(self):
+    #     model_reg = pcl.SampleConsensusModelRegistration(self.p)
+    #     ransac = pcl.RandomSampleConsensus (model_reg)
+    #     ransac.set_DistanceThreshold (.01)
+    #     ransac.computeModel()
+    #     inliers = ransac.get_Inliers()
+    #     
+    #     # print(str(len(inliers)))
+    #     self.assertNotEqual(len(inliers), 0)
 
 
-    def test_Test(self):
+    def test_ModelSphere(self):
+        model_s = pcl.SampleConsensusModelSphere(self.p)
+        ransac = pcl.RandomSampleConsensus (model_s)
+        ransac.set_DistanceThreshold (.01)
+        ransac.computeModel()
+        inliers = ransac.get_Inliers()
+        
+        # print(str(len(inliers)))
+        self.assertNotEqual(len(inliers), 0)
+        
+        final = pcl.PointCloud()
+        
+        if len(inliers) != 0:
+            finalpoints = np.zeros((len(inliers), 3), dtype=np.float32)
+            
+            for i in range(0, len(inliers)):
+                finalpoints[i][0] = self.p[inliers[i]][0]
+                finalpoints[i][1] = self.p[inliers[i]][1]
+                finalpoints[i][2] = self.p[inliers[i]][2]
+            
+            final.from_array(finalpoints)
+        
+        self.assertNotEqual(final.size, 0)
         pass
 
 
-### SampleConsensusModelSphere ###
-class TestSampleConsensusModelSphere(unittest.TestCase):
-    def setUp(self):
-        self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
+    def test_SampleConsensusModelStick(self):
+        model_st = pcl.SampleConsensusModelStick(self.p)
+        ransac = pcl.RandomSampleConsensus (model_st)
+        ransac.set_DistanceThreshold (.01)
+        ransac.computeModel()
+        inliers = ransac.get_Inliers()
+        
+        # print(str(len(inliers)))
+        self.assertNotEqual(len(inliers), 0)
 
 
-    def test_Test(self):
-        pass
-
-
-### SampleConsensusModelStick ###
-class TestSampleConsensusModelStick(unittest.TestCase):
-    def setUp(self):
-        self.p = pcl.PointCloud(_data)
-        self.smp_con = pcl.RandomSampleConsensus()
-
-
-    def test_Test(self):
-        pass
+    # def testException(self):
+    #     self.assertRaises(TypeError, pcl.RandomSampleConsensus)
+    #     pass
 
 
 def suite():
     suite = unittest.TestSuite()
+    
+    # Sampleconsensus
     suite.addTests(unittest.makeSuite(TestRandomSampleConsensus))
-    suite.addTests(unittest.makeSuite(TestSampleConsensusModel))
-    suite.addTests(unittest.makeSuite(TestSampleConsensusModelCylinder))
-    suite.addTests(unittest.makeSuite(TestSampleConsensusModelLine))
-    suite.addTests(unittest.makeSuite(TestSampleConsensusModelPlane))
-    suite.addTests(unittest.makeSuite(TestSampleConsensusModelRegistration))
-    suite.addTests(unittest.makeSuite(TestSampleConsensusModelSphere))
-    suite.addTests(unittest.makeSuite(TestSampleConsensusModelStick))
 
     return suite
 
