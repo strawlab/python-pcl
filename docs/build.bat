b4219f77a7a3dfb883aff1010d995ab92ba7e612
sirokujira <toru753@gmail.com>
2018-12-04 17:54:13 +0900

    modified docs build batch.(add comment)

--- docs/build.bat
+++ docs/build.bat
@@ -2,6 +2,12 @@
 
 rem generate from '.po' to '.mo' files.
 sphinx-intl build
+rem generate html files.
 make html
-rem jp?
+rem translation jp
+rem https://potranslator.readthedocs.io/ja/latest/readme.html#supported-languages
+rem Unix
 rem make -e SPHINXOPTS="-D language='ja'" html
+rem Windows
+rem set SPHINXOPTS=-D language=ja
+rem make html
