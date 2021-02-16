a6a3825790affac36c4f6a7a4658e6682866aece
Ben Zinberg <bzinberg@alum.mit.edu>
2021-02-16 13:26:04 -0500

    Local modifications attempting to get this to install with Ubuntu packages `libpcl-dev` (depends on vtk6)

--- setup.py
+++ setup.py
@@ -723,7 +723,10 @@ else:
             vtk_library_dir = os.path.join('/usr/lib')
         elif pcl_version == '-1.8':
             # pcl 1.8.0/1?(Ubuntu 18.04)(melodic)
-            vtk_version = '7.0'
+            # BZINBERG EDIT:
+            print('Overriding VTK version due to libpcl-dev depending on vtk6')
+            vtk_version = '6.2'
+            # vtk_version = '7.0'
             # pcl 1.8.1?
             # vtk_version = '8.0'
             vtk_include_dir = os.path.join('/usr/include/vtk-' + vtk_version)
